3465b977081d86e6da7c2563a38c9ed7af5c66f9
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2019-01-14 14:08:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
+
+
 const questions = [
   {questionText: "Lightning never strikes in the same place twice", answer: false},
   {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
-]
+];
 
 let question;
+
+function appendQuestion(question) {
+  let questionContainerDiv = document.querySelector(".question-container");
+  questionContainerDiv.innerHTML = question.questionText;
+}
+
+function askQuestionThen(time) {
+  question = questions[0];
+  var promise1 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve(question);
+    }, time);
+  });
+  return promise1;
+}
+
+function removeQuestion() {
+  return new Promise(function () {
+    let questionContainerDiv = document.querySelector(".question-container");
+    questionContainerDiv.innerHTML = '';
+    toggleTrueAndFalseButtons();
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  appendQuestion(question);
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function (button) {
+    button.classList.toggle("hide");
+  })
+}
+
+function displayQuestionOnClick() {
+  appendQuestion(question);
+  trueAndFalseButtons();
+  toggleTrueAndFalseButtons();
+}
